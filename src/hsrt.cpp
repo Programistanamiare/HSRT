e34a765723938dd8bd20b1a6e2f522eb03d12683
Programistanamiare <88616887+Programistanamiare@users.noreply.github.com>
2025-07-22 17:34:38 +0200

    First commit

---
+++ src/hsrt.cpp
@@ -0,0 +1,135 @@
+#include "hsrt.hpp"
+
+
+HW_HSRT::HW_HSRT(HardwareSerial& serial, uint8_t rx, uint8_t tx)
+: rxPin{rx},
+  txPin{tx},
+  baudrate{9600},
+  waitByteTime{0.f},
+  serial{serial}
+{}
+
+
+void HW_HSRT::init(uint16_t baudrate)
+{
+  this->baudrate = baudrate;
+  serial.end();
+  pinMode(txPin, OUTPUT);
+  digitalWrite(txPin, LOW);
+  pinMode(rxPin, INPUT);
+  waitByteTime = (10.f / baudrate) * 1000.f; 
+}
+
+bool HW_HSRT::isPeerReady(void) const
+{
+  return HIGH == digitalRead(rxPin);
+}
+
+int HW_HSRT::write(uint8_t* buffer, uint16_t size, unsigned long timeout)
+{
+  if (nullptr == buffer) return -2; // sprawdzenie czy wskaznik do bufora danych nie jest pusty
+  if (isPeerReady()) return -1; // sprawdzenie czy podlaczone urzadzenie nie chce nadawac
+  digitalWrite(txPin, HIGH); // wyslanie sygnalu chęci nadawania
+  unsigned long t0 = millis(); 
+  while(!isPeerReady()) // dopoki urzadzenie odbiorcze nie odpowie
+  {
+    if (millis() - t0 > timeout) // jesli minal czas oczekiwania na odpowiedz
+    {
+      digitalWrite(txPin, LOW); // odwolanie sygnału chęci nadawania
+      return -3;
+    }
+  }
+  serial.begin(baudrate); // uruchomienie portu szeregowego
+  serial.write(buffer, size); // wyslanie danych z bufora
+  serial.flush(); // odczekanie na wyslanie wszystkich danych z bufora
+  serial.end(); // wylaczenie portu szeregowego
+  digitalWrite(txPin, LOW); // zresetowanie sygnalu chęci nadawania
+  delay(2); // nie ruszaj tego to musi kurwa być!!!
+  return 0;
+}
+
+int HW_HSRT::read(uint8_t* buffer, uint16_t size)
+{
+  if (nullptr == buffer) return -2; // sprawdznie czy wskaznik do bufora danych nie jest pusty
+  if (!isPeerReady()) return -1; // sprawdzenie czy podlaczone urzadzenie chce nadawac
+  serial.begin(baudrate); // wlaczenie portu szeregowego
+  digitalWrite(txPin, HIGH); // wyslanie sygnalu odpowiedzi gotowości do odbioru danych
+  delay(waitByteTime * size + 1); // odczekanie na wyslanie przez nadawcę danych
+  size_t bytesRead = 0;
+  while (serial.available() > 0)
+  {
+    buffer[bytesRead++] = serial.read();
+  }
+  // size_t bytesRead = serial.readBytes(buffer, size); // odczytanie danych do bufora
+  serial.end(); // wylaczenie portu szeregowego
+  digitalWrite(txPin, LOW); // zresetowanie sygnału gotowości odbioru
+  return bytesRead;
+}
+
+
+SW_HSRT::SW_HSRT(uint8_t rx, uint8_t tx)
+: rxPin{rx},
+  txPin{tx},
+  baudrate{9600},
+  waitByteTime{0.f},
+  serial{rx, tx}
+{}
+
+
+void SW_HSRT::init(uint16_t baudrate)
+{
+  this->baudrate = baudrate;
+  serial.end();
+  pinMode(txPin, OUTPUT);
+  digitalWrite(txPin, LOW);
+  pinMode(rxPin, INPUT);
+  waitByteTime = (10.f / baudrate) * 1000.f;
+  serial.setTimeout(10);
+}
+
+bool SW_HSRT::isPeerReady(void) const
+{
+  return HIGH == digitalRead(rxPin);
+}
+
+int SW_HSRT::write(uint8_t* buffer, uint16_t size, unsigned long timeout)
+{
+  if (nullptr == buffer) return -2; // sprawdzenie czy wskaznik do bufora danych nie jest pusty
+  if (isPeerReady()) return -1; // sprawdzenie czy podlaczone urzadzenie nie chce nadawac
+  digitalWrite(txPin, HIGH); // wyslanie sygnalu chęci nadawania
+  unsigned long t0 = millis(); 
+  while(!isPeerReady()) // dopoki urzadzenie odbiorcze nie odpowie
+  {
+    if (millis() - t0 > timeout) // jesli minal czas oczekiwania na odpowiedz
+    {
+      digitalWrite(txPin, LOW); // odwolanie sygnału chęci nadawania
+      return -3;
+    }
+  }
+  serial.begin(baudrate); // uruchomienie portu szeregowego
+  serial.write(buffer, size); // wyslanie danych z bufora
+  serial.end(); // wylaczenie portu szeregowego
+  digitalWrite(txPin, LOW); // zresetowanie sygnalu chęci nadawania
+  delay(2); // nie ruszaj tego to musi kurwa być!!!
+  return 0;
+}
+
+int SW_HSRT::read(uint8_t* buffer, uint16_t size)
+{
+  if (nullptr == buffer) return -2; // sprawdznie czy wskaznik do bufora danych nie jest pusty
+  if (!isPeerReady()) return -1; // sprawdzenie czy podlaczone urzadzenie chce nadawac
+  serial.begin(baudrate); // wlaczenie portu szeregowego
+  digitalWrite(txPin, HIGH); // wyslanie sygnalu odpowiedzi gotowości do odbioru danych
+  interrupts();
+  //delay(waitByteTime * 7 + 1); // odczekanie na wyslanie przez nadawcę danych
+  // size_t bytesRead = 0;
+  // for (int i = 0; i < 7; ++i)
+  // {
+    //   Serial.write(serial.read());
+    // }
+  size_t bytesRead = serial.readBytes(buffer, size); // odczytanie danych do bufora
+    Serial.write(bytesRead);
+    digitalWrite(txPin, LOW); // zresetowanie sygnału gotowości odbioru
+  serial.end(); // wylaczenie portu szeregowego
+  return bytesRead;
+}
---
+++ src/hsrt.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+class HW_HSRT
+{
+public:
+  HW_HSRT(HardwareSerial& serial, uint8_t rx, uint8_t tx);
+  /**
+   * Inicjalizacja obiektu.
+   * @param baudrate szybkosc komunikacji
+   * @retval None
+   */
+  void init(uint16_t baudrate);
+  /**
+   * Odczyt danych.
+   * @param buffer bufor pamięci, do którego zostaną zapisane odczytane dane
+   * @param size rozmiar bufora
+   * @retval -2 wskaznik do bufora danych jest pust
+   * @retval -1 urządzenie nadawcze nie ma wysłało sygnału chęci komunikacji
+   * @retval >= 0 ilość odebranych bajtów danych.
+   */
+  int read(uint8_t* buffer, uint16_t size);
+  /**
+   * Wysyłanie danych.
+   * @param buffer bufor pamięci, z którego zostaną wysłane dane
+   * @param size ilość danych do wysłania w bajtach
+   * @param timeout czas, po którym zakończy się oczekiwanie na odpowiedź od urządzenia odbiorczego
+   * @retval -3 upłynął czas oczekiwania na odpowiedź od urządzenia odbiorczego
+   * @retval -2 wskaznik do bufora danych jest pust
+   * @retval -1 urządzenie odbiorcze nadało sygnał chęci przesyłu danych (należy obsłużyć ich odczyt, a później wysłać swoje dane)
+   * @retval 0 pomyślnie wysłano dane.
+   */
+  int write(uint8_t* buffer, uint16_t size, unsigned long timeout = 200UL);
+  bool isPeerReady(void) const;
+
+private:
+  uint8_t rxPin;
+  uint8_t txPin;
+  uint16_t baudrate;
+  float waitByteTime;
+  HardwareSerial& serial;
+};
+
+
+class SW_HSRT
+{
+public:
+  SW_HSRT(uint8_t rx, uint8_t tx);
+  /**
+   * Inicjalizacja obiektu.
+   * @param baudrate szybkosc komunikacji
+   * @retval None
+   */
+  void init(uint16_t baudrate);
+  /**
+   * Odczyt danych.
+   * @param buffer bufor pamięci, do którego zostaną zapisane odczytane dane
+   * @param size rozmiar bufora
+   * @retval -2 wskaznik do bufora danych jest pust
+   * @retval -1 urządzenie nadawcze nie ma wysłało sygnału chęci komunikacji
+   * @retval >= 0 ilość odebranych bajtów danych.
+   */
+  int read(uint8_t* buffer, uint16_t size);
+  /**
+   * Wysyłanie danych.
+   * @param buffer bufor pamięci, z którego zostaną wysłane dane
+   * @param size ilość danych do wysłania w bajtach
+   * @param timeout czas, po którym zakończy się oczekiwanie na odpowiedź od urządzenia odbiorczego
+   * @retval -3 upłynął czas oczekiwania na odpowiedź od urządzenia odbiorczego
+   * @retval -2 wskaznik do bufora danych jest pust
+   * @retval -1 urządzenie odbiorcze nadało sygnał chęci przesyłu danych (należy obsłużyć ich odczyt, a później wysłać swoje dane)
+   * @retval 0 pomyślnie wysłano dane.
+   */
+  int write(uint8_t* buffer, uint16_t size, unsigned long timeout = 200UL);
+  bool isPeerReady(void) const;
+
+private:
+  uint8_t rxPin;
+  uint8_t txPin;
+  uint16_t baudrate;
+  float waitByteTime;
+  SoftwareSerial serial;
+};
